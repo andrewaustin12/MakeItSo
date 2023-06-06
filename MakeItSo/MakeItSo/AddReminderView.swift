149d6645fd3835e81c65e7ea4af4b4eb283e2661
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-06-06 13:32:17 +0200

    refined the ui

--- MakeItSo/MakeItSo/AddReminderView.swift
+++ MakeItSo/MakeItSo/AddReminderView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct AddReminderView: View {
+    enum FocusableField: Hashable {
+        case title
+    }
+    
+    @FocusState
+    private var focusedField: FocusableField?
+    
     @State
     private var reminder = Reminder(title: "")
     
@@ -21,17 +28,33 @@ struct AddReminderView: View {
         dismiss()
     }
     
+    private func cancel() {
+        dismiss()
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
                 TextField("Title", text: $reminder.title)
+                    .focused($focusedField, equals: .title)
             }
+            .navigationTitle("New Reminder")
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button(action: cancel) {
+                        Text("Cancel")
+                    }
+                }
                 ToolbarItem(placement: .confirmationAction) {
                     Button(action: commit) {
                         Text("Add")
                     }
+                    .disabled(reminder.title.isEmpty)
                 }
+            }
+            .onAppear {
+                focusedField = .title
             }
         }
     }
